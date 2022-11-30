fec281096e080076e0105009c0be1f452b3dd5de
Tran Duc <ducl6337@gmail.com>
2022-11-30 18:56:24 +0700

    Update version 2

--- src/authen/RequireAuth.js
+++ src/authen/RequireAuth.js
@@ -3,7 +3,9 @@ import { useLocation } from "react-router-dom";
 import AuthContext from "./AuthContext";
 
 function RequireAuth({ children }) {
+  // eslint-disable-next-line no-unused-vars
   const auth = useContext(AuthContext);
+  // eslint-disable-next-line no-unused-vars
   const location = useLocation();
 
   return children;
